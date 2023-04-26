3d3d0d143f5bf1cbe3786d583694d55960d315bb
Angel <71894490+AngelS2k@users.noreply.github.com>
2023-04-26 01:15:16 -0400

    borrado

--- test/electivo_2023/README.md
+++
@@ -1,2 +0,0 @@
-# electivo_2023
-Proyecto especialidad 01 Universidad Autonoma 2023
--- test/electivo_2023/core/static/lib/inner-services.js
+++
@@ -1,6 +0,0 @@
-import {setServicesElements} from "./utils";
-
-window.addEventListener("load", () => {
-    const id = parseInt(new URL(window.location.href).pathname.match(/\d+/gm)[0]);
-    setServicesElements(id);
-})
